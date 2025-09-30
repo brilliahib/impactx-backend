86bea10ec97cf00cfb8cad4c2f2f6a2ca821f4cf
brilliahib <brilliahib21@gmail.com>
2025-09-30 22:13:53 +0700

    fix(activity): change activity category to json

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -24,6 +24,10 @@ class Activity extends Model
         'activity_category',
     ];
 
+    protected $casts = [
+        'activity_category' => 'array',
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
