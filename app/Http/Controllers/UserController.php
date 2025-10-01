7d8e5e39bc57afbd6f0adee46126c42ea728bc79
brilliahib <brilliahib21@gmail.com>
2025-10-01 19:21:00 +0700

    feat(suggest): get all suggest people

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function suggestUsers()
+    {
+        $user = Auth::user();
+
+        $suggestions = User::where('id', '!=', $user->id)
+            ->whereNotIn('id', $user->followings()->pluck('followed_id'))
+            ->inRandomOrder()
+            ->limit(5)
+            ->get(['id', 'first_name', 'last_name', 'username']);
+
+        return response()->json([
+            'meta' => [
+                'status' => 'success',
+                'statusCode' => 200,
+                'message' => 'User suggestions retrieved successfully.'
+            ],
+            'data' => $suggestions
+        ], 200);
+    }
+
+    public function getAllUsers()
+    {
+        $user = Auth::user();
+
+        $users = User::where('id', '!=', $user->id)
+            ->get(['id', 'first_name', 'last_name', 'username', 'email']);
+
+        return response()->json([
+            'meta' => [
+                'status' => 'success',
+                'statusCode' => 200,
+                'message' => 'All users retrieved successfully.'
+            ],
+            'data' => $users
+        ], 200);
+    }
+}
